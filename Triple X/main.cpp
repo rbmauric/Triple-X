d007e811162c6ef60b774afcb36b36d519900519
rbmauric <35748166+rbmauric@users.noreply.github.com>
2019-11-24 14:41:16 -0800

    Formatting changes, updated game

--- Triple X/main.cpp
+++ Triple X/main.cpp
@@ -8,7 +8,7 @@
 const int MAX = 9;
 const int TRIES = 3;
 
-bool checkArr(int* Arr, int N) {
+bool CheckArr(int* Arr, int N) {
 	int Guess;
 	int GuessNum = N + 1;
 	std::cout << "\n Enter guess " << ": ";
@@ -16,43 +16,45 @@ bool checkArr(int* Arr, int N) {
 
 	if (N == 2) {
 		std::cout << "   Correct. Lock has been picked.\n";
+		std::cin.clear();
+		std::cin.ignore();
 		return true;
 	}
 
 	if (Guess == Arr[N]) {
 		std::cout << "   Correct.";
-		return checkArr(Arr, N + 1);
+		std::cin.clear();
+		std::cin.ignore();
+		return CheckArr(Arr, N + 1);
 	}
 	else {
 		std::cout << "   Incorrect. \n";
+		std::cin.clear();
+		std::cin.ignore();
 		return false;
 	}
 }
 
-int game(int Dif, int LockNum) {
+int Game(int Dif, int LockNum) {
 	srand(time(NULL));
 
 	if (LockNum == MAX)
 		std::cout << "You have reached the final vault. \n";
 	else
-	std::cout << "\n\n\tSTARTING LOCKNUM: " << LockNum << std::endl;
-	std::cout << "\tThe number pad reads from 0 to " << Dif << ".\n";
+		std::cout << "\n\n\tSTARTING LOCKNUM: " << LockNum + 1 << std::endl;
 
-	const int Num1 = (rand() % Dif + 1);
-	const int Num2 = (rand() % Dif + 1);
-	const int Num3 = (rand() % Dif + 1);
+	const int Num1 = (rand() % Dif + Dif);
+	const int Num2 = (rand() % Dif + Dif);
+	const int Num3 = (rand() % Dif + Dif);
 
 	int arr[3] = { Num1, Num2, Num3 };
 
-	for (int i = 0; i < TRIES; i++) {
-		if (i == 1)
-			std::cout << " Hint: the numbers add to " << (Num1 + Num2 + Num3) << "\n";
-		else if (i == 2)
-			std::cout << " Hint: the numbers multiply to " << (Num1 * Num2 * Num3) << "\n";
-			
+	std::cout << " \tHint: the numbers add to " << (Num1 + Num2 + Num3) << "\n";
+	std::cout << " \tHint: the numbers multiply to " << (Num1 * Num2 * Num3) << "\n";
 
-		if (checkArr(arr, 0) == true)
-			return game(Dif + 1, LockNum + 1);
+	for (int i = 0; i < TRIES; i++) {
+		if (CheckArr(arr, 0) == true)
+			return Game(Dif + 1, LockNum + 1);
 		else
 			std::cout << "\n " << "Tries left: " << (TRIES - i - 1) << "\n";
 	}
@@ -65,8 +67,9 @@ int main() {
 	int Difficulty = 2;
 	
 	std::cout << "WELCOME TO LOCKPICKER! \n   YOU'RE MISSION: BREAK ALL THE LOCKS AND GET THE MONEY AT THE FINAL VAULT. \n";
+	std::cout << "   Each lock has 3 possible numbers. You only have three tries before the police are alerted!\n";
 	
-	if (game(Difficulty, 0) == true) 
+	if (Game(Difficulty, 0) == true) 
 		std::cout << "CONGRATULATIONS. YOU HAVE BROKEN THE FINAL VAULT. YOU WIN!";
 
 	return 0;
